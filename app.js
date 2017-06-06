a218704702bcba5112b20cc3b6f51a287870f57c
Ian Waites <ian.waites@gmail.com>
2017-06-06 14:32:54 -0700

    Currently working code - adding before I break something

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var hasDogs;
+
+var hasDogs = prompt('Does Ian have any dogs?');
+
+var lowerCase = hasDogs.toLowerCase();
+
+  if (lowerCase === 'yes') {
+    alert('Awesome! Ian has 3 dogs!');
+  } else {
+    alert('Ahhh, nope! Ian has 3 dogs!');
+  }
