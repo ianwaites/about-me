dd97918a7b61efa970cef2135bb727dfb837a882
Ian Waites <ian.waites@gmail.com>
2017-06-06 17:30:52 -0700

    lots of CSS additions - added nav and some ideas for layout

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 "use strict";
 
-var hasDogs;
-
-var hasDogs = prompt('Does Ian have any dogs?');
+/*var hasDogs = prompt('Does Ian have any dogs?');
 
 var lowerCase = hasDogs.toLowerCase();
 
-  if (lowerCase === 'yes') {
+  if (lowerCase === 'yes' || lowerCase === 'y') {
     alert('Awesome! Ian has 3 dogs!');
   } else {
     alert('Ahhh, nope! Ian has 3 dogs!');
