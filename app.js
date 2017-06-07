28aa5a1bebd1bdf960456a64ce2d4c4821bc561c
Ian Waites <ian.waites@gmail.com>
2017-06-06 17:52:13 -0700

    added the html files for the individual courses for the nav drop down

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*var hasDogs = prompt('Does Ian have any dogs?');
+var hasDogs = prompt('Does Ian have any dogs? Yes/No or Y/N');
 
 var lowerCase = hasDogs.toLowerCase();
 
@@ -9,3 +9,52 @@ var lowerCase = hasDogs.toLowerCase();
   } else {
     alert('Ahhh, nope! Ian has 3 dogs!');
   }
+
+console.log('asks question about ian\'s dogs and inserts the users input')
+
+var listen = prompt('Does Ian listen to music? Yes/No or Y/N');
+
+var lowerCase = listen.toLowerCase();
+
+  if (lowerCase === 'yes' || lowerCase === 'y') {
+    alert('Awesome, yeah! Ian listens to all sorts of music!');
+  } else {
+    alert('What!?! Of course Ian listens to music!');
+  }
+
+console.log('asks question about ian listening to music and accepts the users input')
+
+var hasDogs = prompt('Does Ian have any dogs? Yes/No or Y/N');
+
+var lowerCase = hasDogs.toLowerCase();
+
+  if (lowerCase === 'yes' || lowerCase === 'y') {
+    alert('Awesome! Ian has 3 dogs!');
+  } else {
+    alert('Ahhh, nope! Ian has 3 dogs!');
+  }
+console.log('asks question about ian\'s dogs and inserts the users input')
+
+var hasDogs = prompt('Does Ian have any dogs?');
+
+var lowerCase = hasDogs.toLowerCase();
+
+  if (lowerCase === 'yes' || lowerCase === 'y') {
+    alert('Awesome! Ian has 3 dogs!');
+  } else {
+    alert('Ahhh, nope! Ian has 3 dogs!');
+  }
+
+console.log('asks question about ian\'s dogs and inserts the users input')
+
+var hasDogs = prompt('Does Ian have any dogs?');
+
+var lowerCase = hasDogs.toLowerCase();
+
+  if (lowerCase === 'yes' || lowerCase === 'y') {
+    alert('Awesome! Ian has 3 dogs!');
+  } else {
+    alert('Ahhh, nope! Ian has 3 dogs!');
+  }
+  
+console.log('asks question about ian\'s dogs and inserts the users input')
