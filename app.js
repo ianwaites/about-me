3f05977e9ca5f56be1fba4f3c4c4f3eb9d9d2357
Cameron <cameron.32090@gmail.com>
2017-06-08 13:58:05 -0700

    fork

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Question 1
+function question1() {
 var hasDogs = prompt('Does Ian have any dogs? Yes/No or Y/N');
 var lowerCase = hasDogs.toLowerCase();
 if (lowerCase === 'yes' || lowerCase === 'y') {
@@ -9,8 +10,10 @@ if (lowerCase === 'yes' || lowerCase === 'y') {
   alert('Ahhh, nope! Ian has 3 dogs!');
 }
 console.log('asks question about ian\'s dogs and inserts the users input');
-
+}
+question1();
 // Question 2
+function question2() {
 var listen = prompt('Does Ian listen to music? Yes/No or Y/N');
 var listenCase = listen.toLowerCase();
 
@@ -20,8 +23,10 @@ if (listenCase === 'yes' || listenCase === 'y') {
   alert('What!?! Of course Ian listens to music!');
 }
 console.log('asks question about ian listening to music and accepts the users input');
-
+}
+question2();
 // Question 3
+function question3() {
 var favColor = prompt('Does Ian have a favorite color? Yes/No or Y/N');
 var colorCase = favColor.toLowerCase();
 if (colorCase === 'yes' || colorCase === 'y') {
@@ -30,8 +35,11 @@ if (colorCase === 'yes' || colorCase === 'y') {
   alert('Incorrect.');
 }
 console.log('asks question about having a favorite color');
+}
+question3();
 
 // Question 4
+function question4() {
 var ianDrive = prompt('Does Ian drive to school? Please answer Yes/No or Y/N');
 var driveCase = ianDrive.toLowerCase();
 if (driveCase === 'no' || driveCase === 'n') {
@@ -40,8 +48,10 @@ if (driveCase === 'no' || driveCase === 'n') {
   alert('Incorrect! Ian takes the bus to class.');
 }
 console.log('asks question about if Ian drives to class and makes No or N the correct answer');
-
+}
+question4();
 // Question 5
+function question5() {
 var ianBeer = prompt('Does Ian drink a bunch of beer before class? Please answer Yes/No or Y/N');
 var drinkCase = ianBeer.toLowerCase();
 if (drinkCase === 'no' || drinkCase === 'n') {
@@ -49,10 +59,12 @@ if (drinkCase === 'no' || drinkCase === 'n') {
 } else {
   alert('Nice try... Makes me think you enjoy drinking some beer in the morning!');
 }
-
+}
 console.log('asks question about ian consuming alchohol before class and looks for No or N as the correct answer');
+question5();
 
 // Begin lab question 6
+function question6() {
 var counter = 4;
 while (counter > 0){
   var ianDrinks = parseInt(prompt('How many 40oz of beers did Ian drink before class? Make sure you are answering the question using numbers (ex: "1", "2", "3", etc...)'));
@@ -68,8 +80,10 @@ while (counter > 0){
 
   console.log('asks question about ian consuming alchohol before class and gives the user 4 guesses');
 }
-
+}
+question6();
 // Begin Question 7
+function question7() {
 var totalRight = 0;
 var tries = 6;
 var answers = false;
@@ -99,29 +113,31 @@ while (tries > 0){
 if (tries === 0){
   alert('Sorry you fail.');
 }
+}
+question7();
 
-// //Teachers code
-//
-// var states = [];
-// var guess = 6;
-//
-// while(guess > 0) {
-//   guess--;
-//   var stateanswer = prompt();
-//
-//   for( var i = 0; i < states.length; i++) {
-//     if (stateanswer === states[i] || stateanswer === stateabb[i]){
-//       alert();
-//       guess = -1;
-//       break;
-//     }
-//   }
-// }
-//
-// if(guess > 0){
-//   alert('nope wrong you have this many guesses left');
-// }
-//
-// if(guess ){
-//
-// }
+// // //Teachers code
+// //
+// // var states = [];
+// // var guess = 6;
+// //
+// // while(guess > 0) {
+// //   guess--;
+// //   var stateanswer = prompt();
+// //
+// //   for( var i = 0; i < states.length; i++) {
+// //     if (stateanswer === states[i] || stateanswer === stateabb[i]){
+// //       alert();
+// //       guess = -1;
+// //       break;
+// //     }
+// //   }
+// // }
+// //
+// // if(guess > 0){
+// //   alert('nope wrong you have this many guesses left');
+// // }
+// //
+// // if(guess ){
+// //
+// // }
